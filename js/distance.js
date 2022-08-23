97911e7f73895229603e6f6c407f21287a9e297b
Bernard Lewis Cardona <110211585+bernardlewiscardona@users.noreply.github.com>
2022-08-23 15:16:32 +0800

    Update distance.js

--- js/distance.js
+++ js/distance.js
@@ -1,5 +1,5 @@
 //lat1,lon1 = Coordinates sample for Manila (Fort Santiago)
-//lat2,lon2 =Coordinates sample for Manila (Palawan)
+//lat2,lon2 = Coordinates sample for Palawan
 function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
     var R = 6371; // Radius of the earth in km
     var dLat = deg2rad(lat2-lat1);  // deg2rad below
