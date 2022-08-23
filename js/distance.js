ed5e80b4d1727e516dbb8ec79903ec45d64704d6
Bernard Lewis Cardona <110211585+bernardlewiscardona@users.noreply.github.com>
2022-08-23 15:16:05 +0800

    Create distance.js

---
+++ js/distance.js
@@ -0,0 +1,15 @@
+//lat1,lon1 = Coordinates sample for Manila (Fort Santiago)
+//lat2,lon2 =Coordinates sample for Manila (Palawan)
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    console.log(d);
+}
+getDistanceFromLatLonInKm(14.59440662328432, 120.97040364542354,9.44510240959885, 118.39514690416448);
