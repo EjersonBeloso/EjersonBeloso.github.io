6f2b245f63f324ddf9116d30e112b252d2f6b890
Bernard Lewis Cardona <110211585+bernardlewiscardona@users.noreply.github.com>
2022-08-23 15:54:46 +0800

    Update distance.js

--- js/distance.js
+++ js/distance.js
@@ -1,15 +1,21 @@
 //lat1,lon1 = Coordinates sample for Manila (Fort Santiago)
 //lat2,lon2 = Coordinates sample for Palawan
-function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2-lat1);  // deg2rad below
-    var dLon = deg2rad(lon2-lon1); 
-    var a = 
-      Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-      Math.sin(dLon/2) * Math.sin(dLon/2); 
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    var d = R * c; // Distance in km
-    console.log(d);
+function CalcDistanceBetween(lat1, lon1, lat2, lon2) {
+  //Radius of the earth in:  1.609344 miles,  6371 km  | var R = (6371 / 1.609344);
+  var R = 6371; // Radius of earth in Miles 
+  var dLat = toRad(lat2-lat1);
+  var dLon = toRad(lon2-lon1); 
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * 
+          Math.sin(dLon/2) * Math.sin(dLon/2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c;
+  console.log(d);
 }
-getDistanceFromLatLonInKm(14.59440662328432, 120.97040364542354,9.44510240959885, 118.39514690416448);
+
+function toRad(Value) {
+  /** Converts numeric degrees to radians */
+  return Value * Math.PI / 180;
+}
+
+CalcDistanceBetween(14.59440662328432, 120.97040364542354,9.44510240959885, 118.39514690416448);
